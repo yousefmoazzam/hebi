d658381649798749c1690d9a1cbac1424bd3eff2
Dan Nixon <dan@dan-nixon.com>
2019-03-11 08:34:03 +0000

    Don't run tests on container startup
    Caused issues with Kubernetes deployment due to permissions.

--- webservice/run.sh
+++ webservice/run.sh
@@ -2,7 +2,4 @@
 
 export PYTHONPATH=/webservice:$PYTHONPATH
 
-# Run unit tests first, launch server if tests pass
-cd /webservice && \
-    pytest && \
-    python -m webservice /webservice/config_dev.json
+python -m webservice /webservice/config_dev.json
