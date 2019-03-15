73d8ce6a2345b629be891c83576c79b055710402
Dan Nixon <dan@dan-nixon.com>
2019-03-15 10:28:02 +0000

    Document Docker build script

--- build_docker_images.sh
+++ build_docker_images.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+# Builds Docker images for Savu API (-api) and NGINX (-web) containers.
+
+ORG="dannixon"
+PROJECT="hebi"
+
 docker build \
-  --tag dannixon/hebi-web \
+  --tag "${ORG}/${PROJECT}-web" \
   web
 
 docker build \
-  --tag dannixon/hebi-api \
+  --tag "${ORG}/${PROJECT}-api" \
   api
